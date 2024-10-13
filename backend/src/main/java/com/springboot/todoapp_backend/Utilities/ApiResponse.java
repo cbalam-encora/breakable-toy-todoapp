201e4aa24f01c61d5c818ff38b55e68c48b53513
RaulUrbina <raul.urbina@encora.com>
2024-10-12 20:30:05 -0600

    [BACKEND] [VALIDATION] Improved response structure

---
+++ backend/src/main/java/com/springboot/todoapp_backend/Utilities/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.springboot.todoapp_backend.Utilities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    public int status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data){
+        return new ApiResponse<>(HttpStatus.OK.value(), message, data);
+    }
+    public static <T> ApiResponse<T> error(String message, HttpStatus status){
+        return new ApiResponse<>(status.value(), message, null);
+    }
+
+}
